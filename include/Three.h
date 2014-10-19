747f280b99a6a7aa506eb24daaf344d9ddbd2a17
Seth Gibson <seth.gibson@intel.com>
2014-10-18 17:31:38 -0700

    starting on three -sG

--- include/Three.h
+++ include/Three.h
@@ -1,4 +1,24 @@
 #ifndef __THREE__
 #define __THREE__
 
+class ThreeLayer
+{
+public:
+	ThreeLayer();
+	~ThreeLayer();
+private:
+};
+
+class ThreeControl
+{
+public:
+	ThreeControl();
+	~ThreeControl();
+
+private:
+	void step();
+	void display();
+};
+
+typedef std::unique_ptr<ThreeControl> ThreeControlRef;
 #endif
